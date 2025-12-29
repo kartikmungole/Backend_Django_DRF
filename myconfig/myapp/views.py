3938986a18518606a452a1b2d9ec4583e15f2000
kartik-blooprint <kartik.m@blooprint.in>
2025-12-29 13:48:02 +0530

    project setup and helth checkup don for api view

---
+++ myconfig/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def health_check(request):
+    return Response({
+        "status": "ok",
+        "message": "Backend is running"
+    })
